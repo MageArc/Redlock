c8f40d611874fdf74c7d6312ec2086348055c720
chenyj <chenyongjun@rmitec.cn>
2018-06-06 16:02:34 +0800

    common

---
+++ src/main/java/com/morthan/redlock/RedlockApplication.java
@@ -0,0 +1,12 @@
+package com.morthan.redlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedlockApplication.class, args);
+    }
+}
---
+++ src/main/java/com/morthan/redlock/service/RedisLocker.java
@@ -0,0 +1,46 @@
+package com.morthan.redlock.service;
+
+import com.morthan.redlock.exception.UnableToAquireLockException;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * <p>名称 :  </p>
+ * <p>方法 :  </p>
+ * <p>版本 : 1.0 </p>
+ * <p>作者 : 陈勇军 </p>
+ * <p>日期 : 2018/6/6  15:51 </p>
+ */
+@Component
+public class RedisLocker  implements DistributedLocker{
+
+    private final static String LOCKER_PREFIX = "lock:";
+
+    @Autowired
+    RedissonConnector redissonConnector;
+    @Override
+    public <T> T lock(String resourceName, AquiredLockWorker<T> worker) throws InterruptedException, UnableToAquireLockException, Exception {
+
+        return lock(resourceName, worker, 100);
+    }
+
+    @Override
+    public <T> T lock(String resourceName, AquiredLockWorker<T> worker, int lockTime) throws UnableToAquireLockException, Exception {
+        RedissonClient redisson= redissonConnector.getClient();
+        RLock lock = redisson.getLock(LOCKER_PREFIX + resourceName);
+        // Wait for 100 seconds seconds and automatically unlock it after lockTime seconds
+        boolean success = lock.tryLock(100, lockTime, TimeUnit.SECONDS);
+        if (success) {
+            try {
+                return worker.invokeAfterLockAquire();
+            } finally {
+                lock.unlock();
+            }
+        }
+        throw new UnableToAquireLockException();
+    }
+}
